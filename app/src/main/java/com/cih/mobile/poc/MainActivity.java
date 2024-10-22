76337d680cda91365ce31810ab245e3b9f20f89e
abdelghani.ammi <abdelghani.ammi@yakeey.com>
2024-10-22 17:13:38 +0100

    fix localhost

--- app/src/main/java/com/cih/mobile/poc/MainActivity.java
+++ app/src/main/java/com/cih/mobile/poc/MainActivity.java
@@ -20,12 +20,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CHANNEL = "com.cih.credit";
-    private static final String API_ENDPOINT = "https://retoolapi.dev/Fg700C/get-api/1";
+    private static final String API_ENDPOINT = "http://localhost:3000/tokens";
     private FlutterEngine flutterEngine;
     private final Executor executor = Executors.newSingleThreadExecutor();
 
@@ -73,8 +74,9 @@ public class MainActivity extends AppCompatActivity {
     private void fetchToken(MethodChannel.Result result) {
         executor.execute(() -> {
             try {
-                String token = getTokenFromApi();
-                runOnUiThread(() -> result.success(token));
+
+                HashMap<String, String> tokens = getTokenFromApi();
+                runOnUiThread(() -> result.success(tokens));
             } catch (IOException e) {
                 runOnUiThread(() -> result.error("NETWORK_ERROR", e.getMessage(), null));
             }
@@ -82,7 +84,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Make API call to get token
-    private String getTokenFromApi() throws IOException {
+    private
+    HashMap<String, String> getTokenFromApi() throws IOException {
         HttpURLConnection connection = (HttpURLConnection) new URL(API_ENDPOINT).openConnection();
         connection.setRequestMethod("GET");
 
@@ -92,16 +95,24 @@ public class MainActivity extends AppCompatActivity {
             while ((line = reader.readLine()) != null) {
                 response.append(line);
             }
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             try {
                 JSONObject jsonResponse = new JSONObject(response.toString());
                 Log.d("TOKEN_FETCH", "getTokenFromApi: "+ jsonResponse);
-                return jsonResponse.getString("accessToken");
+
+
+                String accessToken = jsonResponse.getString("accessToken");
+                String refreshToken = jsonResponse.getString("refreshToken");
+
+                HashMap<String, String> tokens = new HashMap<>();
+                tokens.put("accessToken", accessToken);
+//                tokens.put("refreshToken", refreshToken);
+                return tokens;
+
             } catch (JSONException e) {
                 Log.e("TOKEN_FETCH", "Error parsing JSON response", e);
                 return null;
             }
-//            return "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJLamxBaHpjZjFmNFppQUFhdnBacUU3WVhvTDh1N1FjV3h0QzlsTTJaRF80In0.eyJleHAiOjE3Mjg0MzA0NzAsImlhdCI6MTcyODM5NDQ3MCwianRpIjoiYWVhZDBlZTMtNjY0MS00NDgxLTg1ZTItNjUwMzYxZWQyOWNiIiwiaXNzIjoiaHR0cHM6Ly9kZXYta2MueWFrZWV5LmlvL3JlYWxtcy9ZYWtlZXlfTG9hbiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJiMTYyN2U1MC02YzAxLTQwNzYtODlmZi03ZjFmNzU5MTQzYWEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJwb3JjaC1zZXJ2aWNlIiwic2Vzc2lvbl9zdGF0ZSI6IjM3NTM2MTc4LWQzMjMtNDYwYS1hZjdkLTZkYjU3MzI4NWNhNSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cHM6Ly9kZXYtcG9yY2gueWFrZWV5LmlvIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1ZYWtlZXlfTG9hbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIiwic2lkIjoiMzc1MzYxNzgtZDMyMy00NjBhLWFmN2QtNmRiNTczMjg1Y2E1Iiwicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtWWFrZWV5X0xvYW4iXSwicHJlZmVycmVkX3VzZXJuYW1lIjoiKzIxMjY1NTEyNDk2NyIsImdpdmVuX25hbWUiOiJldHMiLCJmYW1pbHlfbmFtZSI6InRlc3QifQ.qWVZu0C6EDPuwqIU6WTe4zv7AyW-5tTjvfAYZJrp6Dhbp3CfcQ9pP3oLksD6DCyEOlsC3kU40AOzTl-ngpf_vbcF7CMo0kT0QqIgxdCU4yxWU9UUDpONr_usfz6TXe7Du_aMHFOCVzpHYUi2Oa87Va3hnba_7nvSvAMbO4o7tfin49qfqjtX1NlO6V0xTOA_FwMmaVToRDqwCgs5TYDaOc_LlBlTEOrp06sqAaZLFrIte1Ra-bcoOk4ODZSXFzOersUJiouSSy78B3pW7TUSX84LyC2QR6AI5y7BUG68yJCMbAhsa3kZnVxNPKschADUvzMg_ypqQlHyfCqZl_4dGg";
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
